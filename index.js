e1a81f9cae2d2f8a0427cf13abbdabc81de65e76
Alexander Lazo <lazoalexander86@gmail.com>
2024-12-17 16:48:02 -0500

    chat

--- index.js
+++ index.js
@@ -5,7 +5,38 @@ const cors = require("cors");
 
 const app = express();
 
+const http = require("http");
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Un usuario se ha conectado");
+
+  socket.on("disconnect", () => {
+    console.log("Un usuario se ha desconectado");
+  });
+
+  socket.on("chat", (msg) => {
+    console.log("mensaje: " + msg);
+
+    io.emit("chat", { text: msg, userId: socket.id });
+  });
+});
+
 conectarDB();
+
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST"],
+};
+app.use(cors(corsOptions));
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +51,6 @@ app.use("/api/platillo", require("./routes/platillo"));
 //crear mesero y obtener token
 app.use("/api/mesero", require("./routes/mesero"));
 
-app.listen(config.port, () =>
+server.listen(config.port, () =>
   console.log(`Servidor corriendo en http://localhost:${config.port}`)
 );
